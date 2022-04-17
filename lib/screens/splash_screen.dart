0f52921a0a09fcd2a0e1977adf0580df073aeda0
Somrit Dasgupta <somritdasgupta7@gmail.com>
2022-04-18 02:58:01 +0530

    Navigation Bar UI Updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fuelit_pilot/screens/components/body.dart';
 import 'package:fuelit_pilot/size_config.dart';
 
@@ -6,6 +7,9 @@ class SplashScreen extends StatelessWidget {
   static String routeName = "/Splash";
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.top]
+        );
     //call needed at starting screen
     SizeConfig().init(context);
     return Scaffold(
